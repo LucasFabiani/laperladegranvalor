c67680bc0c0a8095434f36648e6c8e9d3f79dc1f
LucasFabiani <lucasefabiani@gmail.com>
2026-08-29 00:28:39 -0300

    cambio de url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://LucasFabiani.github.io',
-  base: '/laperladegranvalor',
+  base: '/laperladegranvalor/',
 });
